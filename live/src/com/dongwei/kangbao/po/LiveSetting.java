42b8475fc48635abddcb0eb9ec816326a439ed8d
zhj0921 <623105334@qq.com>
2017-02-27 15:11:27 +0800

    live

---
+++ live/src/com/dongwei/kangbao/po/LiveSetting.java
@@ -0,0 +1,24 @@
+package com.dongwei.kangbao.po;
+
+/** 
+ * @ClassName: LiveSetting
+ * @Company: http://www.dongwei.com/
+ * @Description: 直播配置实体类
+ * @author h 
+ * @date 2017年2月27日 下午2:28:37  
+ */
+public class LiveSetting {
+	
+	
+	private Integer id;
+	private String name;
+	private String value;
+	private Integer int_value;
+	private Integer type;
+	private String remark;
+	private Integer re_value;
+	private Integer sort;
+	private Integer addtime;
+	
+
+}
